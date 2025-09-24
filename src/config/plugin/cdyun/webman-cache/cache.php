b4d9d5c27991f8d9b74b6e760e00ced1cba5acbe
cdyun <121625706@qq.com>
2025-09-24 23:37:45 +0800

    cdyun

--- src/config/plugin/cdyun/webman-cache/cache.php
+++ src/config/plugin/cdyun/webman-cache/cache.php
@@ -1,19 +1,23 @@
 <?php
 return [
     // 默认缓存驱动
-    'default' => 'redis',
+    'default' => 'file',
     // 缓存连接方式配置
-    'stores'  => [
+    'stores' => [
         // redis缓存
         'redis' => [
             // 驱动方式
             'type' => 'redis',
             // 服务器地址
             'host' => '127.0.0.1',
+            // 密码
+            'password' => '',
+            // 端口
+            'port' => 6379,
             // 缓存前缀
             'prefix' => 'cache:',
             // 默认缓存有效期 0表示永久缓存
-            'expire'     => 0,
+            'expire' => 0,
             // Thinkphp官方没有这个参数，由于生成的tag键默认不过期，如果tag键数量很大，避免长时间占用内存，可以设置一个超过其他缓存的过期时间，0为不设置
             'tag_expire' => 86400 * 30,
             // 缓存标签前缀
